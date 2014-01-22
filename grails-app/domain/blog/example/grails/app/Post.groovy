1e0d84a9afb66acf53a4c1ae28cae56e417dd69b
Fatima Casau <fcasau@paradigmatecnologico.com>
2014-01-22 17:43:53 +0100

    set constraints, mappings, events and more

--- grails-app/domain/blog/example/grails/app/Post.groovy
+++ grails-app/domain/blog/example/grails/app/Post.groovy
@@ -5,7 +5,7 @@ class Post {
     String title
     String body
     Date dateCreated // autotimestamp
-    Date dateUpdated // autotimestamp
+    Date lastUpdated // autotimestamp
     /**
      * If you have nullable:false constraints on either dateCreated or lastUpdated,
      * your domain instances will fail validation - probably not what you want.
@@ -15,6 +15,41 @@ class Post {
     Boolean published
 
     static constraints = {
-        // By default, all attributes
+        // By default, all attributes have constraint nullable:false
+        dateCreated nullable: true
+        lastUpdated nullable: true
+        title maxSize: 150, unique:true, blank: false
+        body maxSize: 3000, blank: false
+        // validator: { val,obj -> // val: the value of the property. obj: the object to validate }
     }
+
+    static mapping = {
+        body type: 'text'
+    }
+
+   /* Events:
+    *
+    * Do not attempt to flush the session within an event (such as with obj.save(flush:true)).
+    * Since events are fired during flushing this will cause a StackOverflowError.
+    *
+    */
+
+    def beforeValidate(){
+        this.published =  this.published ?: false
+    }
+
+//    def beforeInsert(){}
+//
+//    def beforeUpdate(){}
+//
+//    def beforeDelete(){}
+//
+//    def afterInsert(){}
+//
+//    def afterUpdate(){}
+//
+//    def afterDelete(){}
+//
+//    def onLoad(){}
+
 }
